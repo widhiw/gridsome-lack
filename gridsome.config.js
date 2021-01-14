0b1ab020fe278279cc6e3bb3c0a4dbd79aa0d1cf
widhiw <widhiw28@gmail.com>
2021-01-14 12:17:55 +0700

    commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,12 +1,4 @@
 const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-
-const postcssPlugins = [
-  tailwind(),
-]
-
-if (process.env.NODE_ENV === 'production')
-  postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
   siteName: 'Gridsome',
