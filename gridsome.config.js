c92b360160f18eb72927d3ffe3330d12596633b0
widhiw <widhiw28@gmail.com>
2021-01-14 12:18:20 +0700

    commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,11 +3,4 @@ const tailwind = require('tailwindcss')
 module.exports = {
   siteName: 'Gridsome',
   siteUrl: 'https://widhiw.github.io',
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
-    },
-  }
 }
